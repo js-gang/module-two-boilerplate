c6ce7657923c3811bad1fbba159dc58be35edf78
= <=>
2016-09-13 12:50:50 +0300

    Loading profile is done.

--- src/main.js
+++ src/main.js
@@ -12,14 +12,31 @@ It will be passed automatically via proxy server
 
 function loadUsers(username) {
   const url = `${API_PROXY_URL}/${GAME}/account/list/?search=${username}`
-  // create request to the url and return a promise
-  return fetch(url)
-  .then(response => response.json())
-  .then(responseJson => responseJson.data);
+  return makeRequest(url);
 }
 
-function renderSpinner(domNode) {
+function loadProfile(accountId) {
+  const url = `${API_PROXY_URL}/${GAME}/account/info/?account_id=${accountId}`;
+  return makeRequest(url)
+  .then(data => data[accountId])
+}
+
+function makeRequest(url) {
+  return fetch(url)
+  .then(response => response.json())
+  .then(responseJson => new Promise(function(resolve, reject) {
+    if (responseJson.status === 'ok') {
+      resolve(responseJson.data);
+    } else {
+      reject(responseJson.error.message);
+    }
+  }));
+}
+
+function toggleSpinner() {
   // clean all content of passed node and then render element with `spinner` classname
+  const spinner = document.querySelector('#spinner');
+  spinner.classList.toggle('show');
 }
 
 function renderSearchResult(accounts) {
@@ -27,10 +44,66 @@ function renderSearchResult(accounts) {
   // Note! it's already exist. See index.html for more info.
   // Each search result item should be rendered
   // inside node with `search-results_item` class name.
+  const userList = accounts.map(renderUser).join('');
+  const node = document.querySelector('#search-results');
+  node.innerHTML = userList;
+  for (let element of document.querySelectorAll('.js-user')) {
+    element.addEventListener('click', handleUserClick);
+  }
+}
+
+function renderUser({nickname, account_id}) {
+  return `
+    <div class="search-results_item js-user" data-id="${account_id}">${nickname}</div>
+  `
+}
+
+function renderUserProfile({nickname, global_rating, statistics}) {
+  const {wins, battles} = statistics.all;
+  const winsPercents = (wins / battles * 100).toFixed(2);
+  const profile = `
+    <div>
+    <h1>${nickname}</h1>
+    <p>Rating: ${global_rating}</p>
+    <p>Battles: ${battles}</p>
+    <p>Wins Percent: ${winsPercents}</p>
+    </div>
+  `
+  document.querySelector('#profile').innerHTML = profile;
+}
+
+function toggleError(errorMessage) {
+  const errorElement = document.querySelector('#error-message');
+  errorElement.innerHTML = errorMessage;
+  errorElement.classList.add('show');
 }
 
 function handleSearchClick(e) {
+  // const query = document.q
+  toggleSpinner();
+  const usernameInput = document.querySelector('#username');
+  const username = usernameInput.value;
+  loadUsers(username)
+  .then((data) => {
+    toggleSpinner();
+    return data;
+  })
+  .then(renderSearchResult)
+  .catch((message) => {
+    toggleSpinner();
+    alert(message);
+  });
+
   console.log('Hello');
+}
+
+function handleUserClick(e) {
+  const userNode = e.target;
+  const accountId = userNode.dataset.id;
+  toggleSpinner();
+  loadProfile(accountId)
+  .then(data => renderUserProfile(data))
+  .then(toggleSpinner);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
