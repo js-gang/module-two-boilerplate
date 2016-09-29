7529887eaf55897248866beff32046a5ac9188a9
Eugene Romanovsky <e.l.romanovsky@gmail.com>
2016-09-29 14:31:55 +0300

    initial application

--- src/main.js
+++ src/main.js
@@ -13,10 +13,39 @@ It will be passed automatically via proxy server
 function loadUsers(username) {
   const url = `${API_PROXY_URL}/${GAME}/account/list/?search=${username}`
   // create request to the url and return a promise
+  return fetch(url)
+  .then(resp => resp.json())
+  .then(resp => {
+      toggleSpinner()
+      renderSearchResult(resp.data)
+  })
 }
 
-function renderSpinner(domNode) {
+function loadProfile(account_id) {
+  const url = `${API_PROXY_URL}/${GAME}/account/info/?account_id=${account_id}`
+  // create request to the url and return a promise
+  return fetch(url)
+  .then(resp => resp.json())
+  .then(resp => renderUserProfile(resp.data[account_id]))
+}
+
+function toggleSpinner(domNode) {
   // clean all content of passed node and then render element with `spinner` classname
+  const spinner = document.querySelector('#spinner')
+  spinner.classList.toggle('show')
+}
+
+function renderUserProfile({ nickname, global_rating, statistics }) {
+  const { wins, battles } = statistics.all
+  const winsPercent = ((wins / battles) * 100).toFixed(2)
+
+  document.querySelector('#profile').innerHTML = `
+    <h1>${nickname} <sup>${global_rating}</sup></h1>
+    <div>
+      <p>Battles: ${battles}</p>
+      <p>Wins percent: ${winsPercent}%</p>
+    </div>
+  `
 }
 
 function renderSearchResult(accounts) {
@@ -24,8 +53,34 @@ function renderSearchResult(accounts) {
   // Note! it's already exist. See index.html for more info.
   // Each search result item should be rendered
   // inside node with `search-results_item` class name.
+  const usersList = document.querySelector('#users-list')
+  console.log(accounts)
+  usersList.innerHTML = accounts.map(({ account_id, nickname }) => {
+      return `<div class="search-results_item js-user" data-id="${account_id}">${nickname}</div>`
+  }).join('')
+
+  for (node of document.querySelectorAll('.js-user')) {
+      node.addEventListener('click', handleUserClick)
+  }
 }
+
+function handleSearchClick() {
+    const searchInput = document.querySelector('#username')
+    const username = searchInput.value
+    const accounts = loadUsers(username)
+    toggleSpinner()
+    renderSearchResult(accounts)
+}
+
+function handleUserClick(e) {
+    const userNode = e.target
+    const accountId = userNode.dataset.id
+    loadProfile(accountId)
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
   // add search button click handler here
+  const button = document.querySelector('#search')
+  button.addEventListener('click',  handleSearchClick)
 })
